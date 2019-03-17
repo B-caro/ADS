f9db0e6403c73f24f30efaa202dd7748d132be64
Jonathan Búcaro <jonathanbucaro7@gmail.com>
2019-03-16 22:34:21 -0600

    Añadiendo solución base
    El dll se incluye en la carpeta "packages"

---
+++ MotoShopRacing/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace MotoShopRacing
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUser = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.txtPass = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.btnIngresar = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.materialDivider1 = new MaterialSkin.Controls.MaterialDivider();
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.btnRegistrar = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.cbRecordarme = new MaterialSkin.Controls.MaterialCheckBox();
+            this.SuspendLayout();
+            // 
+            // txtUser
+            // 
+            this.txtUser.Depth = 0;
+            this.txtUser.Hint = "Usuario";
+            this.txtUser.Location = new System.Drawing.Point(23, 100);
+            this.txtUser.MouseState = MaterialSkin.MouseState.HOVER;
+            this.txtUser.Name = "txtUser";
+            this.txtUser.PasswordChar = '\0';
+            this.txtUser.SelectedText = "";
+            this.txtUser.SelectionLength = 0;
+            this.txtUser.SelectionStart = 0;
+            this.txtUser.Size = new System.Drawing.Size(259, 23);
+            this.txtUser.TabIndex = 0;
+            this.txtUser.UseSystemPasswordChar = false;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Depth = 0;
+            this.txtPass.Hint = "Constraseña";
+            this.txtPass.Location = new System.Drawing.Point(23, 158);
+            this.txtPass.MouseState = MaterialSkin.MouseState.HOVER;
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '\0';
+            this.txtPass.SelectedText = "";
+            this.txtPass.SelectionLength = 0;
+            this.txtPass.SelectionStart = 0;
+            this.txtPass.Size = new System.Drawing.Size(259, 23);
+            this.txtPass.TabIndex = 1;
+            this.txtPass.UseSystemPasswordChar = false;
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Depth = 0;
+            this.btnIngresar.Location = new System.Drawing.Point(157, 198);
+            this.btnIngresar.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Primary = true;
+            this.btnIngresar.Size = new System.Drawing.Size(125, 44);
+            this.btnIngresar.TabIndex = 2;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            // 
+            // materialDivider1
+            // 
+            this.materialDivider1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialDivider1.Depth = 0;
+            this.materialDivider1.Location = new System.Drawing.Point(23, 259);
+            this.materialDivider1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialDivider1.Name = "materialDivider1";
+            this.materialDivider1.Size = new System.Drawing.Size(259, 2);
+            this.materialDivider1.TabIndex = 3;
+            this.materialDivider1.Text = "materialDivider1";
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel1.Location = new System.Drawing.Point(23, 297);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(158, 19);
+            this.materialLabel1.TabIndex = 4;
+            this.materialLabel1.Text = "No tienes una cuenta?";
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Depth = 0;
+            this.btnRegistrar.Location = new System.Drawing.Point(187, 296);
+            this.btnRegistrar.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Primary = true;
+            this.btnRegistrar.Size = new System.Drawing.Size(91, 23);
+            this.btnRegistrar.TabIndex = 5;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            // 
+            // cbRecordarme
+            // 
+            this.cbRecordarme.AutoSize = true;
+            this.cbRecordarme.Depth = 0;
+            this.cbRecordarme.Font = new System.Drawing.Font("Roboto", 10F);
+            this.cbRecordarme.Location = new System.Drawing.Point(27, 206);
+            this.cbRecordarme.Margin = new System.Windows.Forms.Padding(0);
+            this.cbRecordarme.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.cbRecordarme.MouseState = MaterialSkin.MouseState.HOVER;
+            this.cbRecordarme.Name = "cbRecordarme";
+            this.cbRecordarme.Ripple = true;
+            this.cbRecordarme.Size = new System.Drawing.Size(105, 30);
+            this.cbRecordarme.TabIndex = 6;
+            this.cbRecordarme.Text = "Recordarme";
+            this.cbRecordarme.UseVisualStyleBackColor = true;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 340);
+            this.Controls.Add(this.cbRecordarme);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.materialLabel1);
+            this.Controls.Add(this.materialDivider1);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUser);
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.frmLogin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialSingleLineTextField txtUser;
+        private MaterialSkin.Controls.MaterialSingleLineTextField txtPass;
+        private MaterialSkin.Controls.MaterialRaisedButton btnIngresar;
+        private MaterialSkin.Controls.MaterialDivider materialDivider1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private MaterialSkin.Controls.MaterialRaisedButton btnRegistrar;
+        private MaterialSkin.Controls.MaterialCheckBox cbRecordarme;
+    }
+}
+
---
+++ MotoShopRacing/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace MotoShopRacing
+{
+    public partial class frmLogin : MaterialForm
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+            // Create a material theme manager and add the form to manage (this)
+            MaterialSkinManager materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+
+            // Configure color schema
+            materialSkinManager.ColorScheme = new ColorScheme(
+                Primary.Blue400, Primary.Blue500,
+                Primary.Blue500, Accent.LightBlue200,
+                TextShade.WHITE
+            );
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
